df64e39e312719754d0cff260d9514192511c300
MaurizioGame <maurizio.seu.red@bigrock.it>
2023-11-11 02:00:11 +0100

    CameraShake fatto

---
+++ Assets/Scenes/Scripts/CameraShakeTrigger.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShakeTrigger : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+           
+            CameraShake.Invoke();
+            Debug.Log ("Shake");
+        }
+    }
+}
