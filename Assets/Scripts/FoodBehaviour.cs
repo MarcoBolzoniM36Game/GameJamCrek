aeaeae7522c00de928c5f3cfba32cbbea1379669
AlessioMazzapioda <144702921+AlessioMazzapioda@users.noreply.github.com>
2023-11-12 10:38:00 +0100

    MESSI VFX MAURIZIO FAMMI UNA POMPA

--- Assets/Scripts/FoodBehaviour.cs
+++ Assets/Scripts/FoodBehaviour.cs
@@ -5,22 +5,29 @@ using UnityEngine;
 
 public class FoodBehaviour : MonoBehaviour
 {
-
-
+    [SerializeField]
+    private GameObject vfxPrefab;
 
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("DeathZone"))
         {
+
             Destroy(gameObject);
+            CameraShake.Invoke();
         }
         else
         {
             if (other.CompareTag("Player"))
             {
                 Destroy(gameObject);
+                Instantiate(vfxPrefab, transform.position, Quaternion.identity);
+                //if (vfxPrefab != null)
+                //{
+                   
 
+                //}
             }
         }
 
