26d21950e9189961e9d111d4811ef9994de6f188
RaffaelePierro <raffaele.pierro.red@bigrock.it>
2023-11-14 18:23:47 +0100

    dasd

--- Assets/Scripts/SceneManagerRaf.cs
+++ Assets/Scripts/SceneManagerRaf.cs
@@ -6,15 +6,38 @@ using UnityEngine.SceneManagement;
 
 public class SceneManagerRaf : MonoBehaviour
 {
+    public GameObject []pippo;
 
     public void GameOver() { SceneManager.LoadScene("GameOver"); }
     public void Menu() { SceneManager.LoadScene("Menu"); }
     public void Game() 
     {
-        SceneManager.LoadScene("GameScene");
+         
+        for (int i = 0; i < pippo.Length; i++)
+        {
+
+           AudioSource c = GetComponent<AudioSource>();
+                
+            if (i == 0)
+            {
+                c.Play();
+            }
+            else if (i==1)
+            {
+                c.Play();
+            }
+        }
+            //pippo.Play();
+        Invoke("Delay",2f);
+        //SceneManager.LoadScene("GameScene");
     }
     public void Quit()
     {
         Application.Quit();
+    }
+
+    void Delay()
+    {
+        SceneManager.LoadScene("GameScene");
     }
 }
