119a1b63bb4658b4b36e3203eb88f70f942d0e83
PierroRaf97 <raffaelepierro.97@gmail.com>
2023-11-14 19:19:22 +0100

    SoundAggiornato

--- Assets/Scripts/SceneManagerRaf.cs
+++ Assets/Scripts/SceneManagerRaf.cs
@@ -6,30 +6,21 @@ using UnityEngine.SceneManagement;
 
 public class SceneManagerRaf : MonoBehaviour
 {
-    public GameObject []pippo;
+    public AudioSource coin;
+    public AudioSource crek;
 
     public void GameOver() { SceneManager.LoadScene("GameOver"); }
     public void Menu() { SceneManager.LoadScene("Menu"); }
     public void Game() 
     {
-         
-        for (int i = 0; i < pippo.Length; i++)
-        {
+        coin.Play();
+        Invoke("Delay",1.5f);
+    }
 
-           AudioSource c = GetComponent<AudioSource>();
-                
-            if (i == 0)
-            {
-                c.Play();
-            }
-            else if (i==1)
-            {
-                c.Play();
-            }
-        }
-            //pippo.Play();
-        Invoke("Delay",2f);
-        //SceneManager.LoadScene("GameScene");
+    public void QuitSound()
+    {
+        crek.Play();
+        Invoke("Quit",1f);
     }
     public void Quit()
     {
